3f3d97204c219c636c681ca65f0f5202db80cba6
ShabirZ <shabirzahir1202@gmail.com>
2026-02-04 17:32:21 -0500

    updating background for signup component

--- frontend/app/routes/SignupPage.jsx
+++ frontend/app/routes/SignupPage.jsx
@@ -1,8 +1,8 @@
 import { SignupForm } from "~/components/ui/signup-form"
 export default function SignupPage() {
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10 ">
-      <div className="w-full max-w-md border-1 border-slate-50 shadow-lg rounded-lg p-8">
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10 bg-gradient-to-t from-slate-50 via-neutral-400 to-slate-500">
+      <div className="w-full max-w-md rounded-lg p-8 bg-white">
         <SignupForm/>
       </div>
     </div>
