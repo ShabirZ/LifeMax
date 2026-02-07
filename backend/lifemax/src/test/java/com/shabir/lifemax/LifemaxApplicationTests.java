cef2ce53e02398f28a8431f6b5325f1b7f7bf578
ShabirZ <shabirzahir1202@gmail.com>
2026-02-07 17:01:49 -0500

    init spring boot

---
+++ backend/lifemax/src/test/java/com/shabir/lifemax/LifemaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shabir.lifemax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifemaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
